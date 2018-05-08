06ca71ea3d7a3d7256308821db23f0ca1a88011a
samtomcom <sam.comer@gmail.com>
2018-05-08 22:10:54 +0100

    sys args

--- image_sort.py
+++ image_sort.py
@@ -1,15 +1,14 @@
-import sys
-import os
+from PIL import Image
 import hashlib
-from PIL import Image  # uses pillow
+import os
 import shutil
+import sys
 
-#rootdir = sys.argv[1]
-rootdir = '/media/clint/libraries/pictures/wallpapers'
-inputdir = os.path.join(rootdir,'in')
-outputdir = os.path.join(rootdir, 'out')
+rootdir = sys.argv[1]
+#rootdir = '/media/clint/libraries/pictures/wallpapers'
+inputdir = os.path.join(rootdir, sys.argv[2])
+outputdir = os.path.join(rootdir, sys.argv[3])
 
-print(inputdir)
 
 # md5 checksum of file
 def md5(fname):
@@ -34,15 +33,16 @@ def gen_dict():
                 print("Skipping duplicate file: {}".format(full_path))
     return file_dict
 
-def copy(file_dict):
-    aspect_ratios = {1.3: "1.33_4x3",
-                     1.8: "1.78_16x9",
-                     1.0: "1.00_1x1"
+
+def copy_files(file_dict):
+    aspect_ratios = {1.33: "1.33_4x3",
+                     1.78: "1.78_16x9",
+                     1.00: "1.00_1x1"
     }
 
     for check in file_dict:
         file, full_path, x, y = tuple(file_dict[check])
-        aspect_r = round(float(x)/y, 1)
+        aspect_r = round(float(x)/y, 2)
         aspect_path = os.path.join(outputdir, str(aspect_r))
         if aspect_r in aspect_ratios:
             aspect_path = os.path.join(outputdir, aspect_ratios[aspect_r])
@@ -55,5 +55,5 @@ def copy(file_dict):
             shutil.copyfile(full_path, new_path)
 
 
-
-copy(gen_dict())
+d = gen_dict()
+copy_files(d)
